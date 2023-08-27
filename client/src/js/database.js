d42678a5abc4933ab6ee23f3547df1fe7b893dfd
jimen8930 <jimen8930@aol.com>
2023-08-27 19:33:36 -0400

    Added logic in order to install the PWA, added logic to get all the content from the database and accept some content from the database.

---
+++ client/src/js/database.js
@@ -0,0 +1,52 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) =>{
+ 
+  // Create a connection to the database database and version
+  const contactDb = await openDB('jate', 1);
+  
+  // Create a new transaction and specify the database and data privileges.
+    const tx = contactDb.transaction('jate', 'readwrite');
+  
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+  
+  // Use the .add() method
+  const request = store.put({ id: 1, value: content });
+  
+   // Get confirmation of the request.
+   const result = await request;
+   console.log('data saved to db', result);
+  }
+  
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () =>{
+  // Create a connection to the database database and version we want to use.
+  const jateDB = await openDB("jate", 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction("jate", "readonly");
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("result.value", result);
+  return result.value;
+}
+
+initdb();
---
+++ client/src/js/install.js
@@ -0,0 +1,21 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+     return;
+    }
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+  });
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+  }); 
